24e77e68cafae60913540f33f5eb4d3649cdcb57
cmendo <carlos.mendozacarrero@gmail.com>
2024-08-09 23:13:46 -0400

    Se envia buildspec_docker actualizado

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,15 @@
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { MyPipelineStack } from '../lib/pipeline-cdk-stack';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
+
+const app = new cdk.App();
+
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
+const testCdkStack = new AppCdkStack(app, 'test', {});
+
+const pipelineCdkStack = new MyPipelineStack(app, 'pipeline-stack', {
+    ecrRepository: ecrCdkStack.repository,
+});
