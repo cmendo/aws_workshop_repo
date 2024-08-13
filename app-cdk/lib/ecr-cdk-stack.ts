7f4bdc02656a8109d4c3078dbc4cc8f6694df010
cmendo <carlos.mendozacarrero@gmail.com>
2024-08-13 16:48:58 -0400

    Nuevos cambios

---
+++ app-cdk/lib/ecr-cdk-stack.ts
@@ -0,0 +1,12 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+export class EcrCdkStack extends Stack {
+    public readonly repository: ecr.Repository;
+
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props);
+        this.repository = new ecr.Repository(this, 'my_app');
+    }
+}
